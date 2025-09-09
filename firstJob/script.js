b07aaf47daf276a4f823ef355786a6d24ebb30e7
Leyla-Ya <yakubova_leila@bk.ru>
2025-09-10 00:41:02 +0300

    Массивы

--- firstJob/script.js
+++ firstJob/script.js
@@ -1,46 +1,83 @@
 // Задание 1:
 
-const str = "JavaScript is fun!";
+const someNumbers = [3, 4, 6, 11];
+const square = someNumbers.map(item => item * item);
 
-console.log(str.includes("fun"));
-
-
-// Задание 5:
-
-const result = 1 + '1';
-
-console.log(result); // 11 - JavaScript автоматически преобразует типы данных 
-// (там, где это получается). 
-// В нашем случае число преобразовано в строку, 
-// так как один из операндов - строка и использовали оператор "+". 
+console.log(square);
 
 
-// Задание 4:
+// Задание 2:
 
-console.log(null == undefined); // Как я поняла это: при не строгом сравнении
-// сравниваются только значения, т.е null - пустое значение, так и undefined - "пустота",
-// поэтому они считаются равными друг другу и вывод true.
-console.log(null === undefined); // При строгом же сравнении сравнивается больше данных, 
-// начиная от определения типа: тип null - это Null (Object), тип undefined - это Undefined.
-// Типы разные - false.
+const array = [1, 2, 2, 3, 4, 5, 5, 5, 6];
+const newArray = array.filter((item, index) => array.indexOf(item) === index);
+
+console.log(newArray); // [1, 2, 3, 4, 5, 6]
 
 
 // Задание 3:
 
-const firstName = "John";
-const lastName = "Doe";
-const occupation = "software developer";
-const message = `Hello, my name is ${firstName} ${lastName}. I am a ${occupation}.`;
+const numbers = [2, 8, 5];
+const sum = numbers.reduce((total, num) => total + num, 0);
 
-console.log(message);
+console.log(sum); 
 
-// Задание 2:
 
-let name = null;
+// Задание 4:
 
-if (name === null) {
-    console.log("Пользователь не найден.");
-} else {
-    console.log("Добавьте больше данных.")
-} // Не знаю, правильно ли я поняла задачу, но тут вроде как переменная имеет
-// одно из false значений, и условие написала так, что сразу выполнится.
+function reverseArray(arr) {
+    let reversed = [];
+
+    for (let i = arr.length - 1; i >= 0; i--) {
+        reversed.push(arr[i]);
+    }
+    return reversed;
+}
+
+const fruitsArray = ['Груша', 'Банан', 'Арбуз', 'Слива', 'Ананас'];
+console.log(fruitsArray);
+
+const reversed = reverseArray(fruitsArray);
+console.log(reversed); 
+
+
+// Задание 5:
+
+let variableLet = 'Гарри Поттер.';
+const variableConst = 'Властелин колец.';
+
+variableLet = 'Harry Potter.'; // let - переменная, которую можно менять.
+variableConst; // variableConst = ''  - при присваивании нового значения выдает ошибку.
+// const - постоянное значение (константа), поэтому перезапись значения недопустима.
+
+console.log(variableLet);
+console.log(variableConst); 
+
+
+let booksName = ['Гарри Поттер', 'Властелин колец', 'Сумерки'];
+const filmsName = ['Чужой', 'Обитель зла', 'Джон Уик'];
+
+let booksNew = booksName.splice(2,2, 'Парфюмер', 'Мешок с костями');
+
+console.log(booksName);
+
+let booksNew1 = booksName.forEach((bookName, index) => {
+    console.log(`${bookName}: ${index}`)
+})
+
+const filmsNew = filmsName.push('Мгла');
+
+console.log(filmsName);
+console.log(filmsNew);
+
+const filmsNew1 = filmsName.map(filmName => `${filmName} - ${'Просмотрено'}`);
+
+console.log(filmsNew1);
+
+const films = filmsName.slice(2);
+
+console.log(filmsName);
+console.log(films);
+// Здесь же const не "ломается", потому что мы не меняем значение,
+// а лишь дополняем. так как массив ссылочный тип данных, мы обращаемся к ссылке, 
+// где хранятся данные массива и добавляем туда нужные нам данные не перезаписывая 
+// массив по новому.
