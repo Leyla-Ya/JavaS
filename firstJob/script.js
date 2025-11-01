7f066bb5e65935899d76f6d569da6d4560188a40
Leyla-Ya <yakubova_leila@bk.ru>
2025-11-01 15:04:07 +0300

    Взаимодействие JS с DOM

--- firstJob/script.js
+++ firstJob/script.js
@@ -1,21 +1,33 @@
-const aboutMe = {
-  name: 'Лейла',
-  age: 33,
-  hobby: ['Велопрогулки', 'Художественные книги', 'Прокрастинация']
+const paragraph = document.getElementById('text');
+console.log(paragraph.textContent);
+
+const paragraphChange = document.querySelector('p');
+paragraphChange.textContent = '1. Найди элемент на странице по его ID и измени его текстовое содержимое на что-то новое;';
+console.log(paragraphChange.textContent);
+
+const change = document.getElementsByClassName('changes');
+for (let changes of change) {
+
+  changes.style.color = 'yellow';
+  changes.style.background = 'grey';
+
 }
 
-console.log(aboutMe.name, aboutMe.age);
-console.log(aboutMe['hobby']);
+const container = document.getElementById('container');
+container.innerHTML = '<p>3. Напиши код, который создает новый параграф с текстом и добавляет его в конец документа;</p>';
 
-aboutMe.address = 'Махачкала, ул. 26';
-console.log(aboutMe['address']);
+const elementToRemove = document.getElementById('text');
+elementToRemove.parentNode.removeChild(elementToRemove);
 
-aboutMe['hobby'] = ['Прогулки', 'Художественные книги', 'Финансы'];
-console.log(aboutMe.hobby);
+const link = document.querySelector('a');
+link.setAttribute('href', 'https://technium.ru/');
+console.log(link.getAttribute('href'));
 
-delete aboutMe.age;
-console.log(aboutMe['age']);
-console.log(aboutMe.age);
+const newElement = document.createElement('div');
+newElement.textContent = '6. Создай новый элемент, добавь к нему класс и добавь его в DOM;';
+document.body.appendChild(newElement);
 
-console.log(aboutMe.name, aboutMe.address);
-console.log(aboutMe['hobby']);
+newElement.classList.add('created');
+
+newElement.classList.toggle('element');
+console.log(newElement);
